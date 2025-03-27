053336023f315659f1034374be4d6393f46c2011
hassanhajizadeh <hhajizadeh24@gmail.com>
2025-03-28 00:06:48 +0330

    making the text waving animation startover after finishing.

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const container= document.querySelector('.container');
+const container = document.querySelector('.container');
 
-container.innerHTML = container.innerText.split('')
-                        .map((letter,idx)=>`<span style='animation-delay:${idx*25}ms'>${letter}</span>`).join('');
+function animateText() {
+    container.innerHTML = container.innerText.split('')
+        .map((letter, idx) => `<span style='animation-delay:${idx * 25}ms'>${letter}</span>`)
+        .join('');
 
+    const spans = container.querySelectorAll('span');
+    spans[spans.length - 1].addEventListener('animationend', animateText, { once: true });
+}
+
+animateText();
